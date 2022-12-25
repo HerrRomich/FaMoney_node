78877bb7fbd937db562a944daec5222704160674
Roman Smushkevich <karmara.roman@gmail.com>
2022-12-25 15:12:06 +0100

    Web-Pack Versuch.

--- packages/launcher/src/main.ts
+++ packages/launcher/src/main.ts
@@ -3,6 +3,8 @@ import { WinstonModule } from 'nest-winston/dist/winston.module';
 import winston from 'winston';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger({
@@ -17,5 +19,10 @@ async function bootstrap() {
     })
   });
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
