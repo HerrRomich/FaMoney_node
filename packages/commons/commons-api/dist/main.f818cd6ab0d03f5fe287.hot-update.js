98259cdf78cab5a0052453c3694ec10a6cc173a4
Roman Smushkevich <karmara.roman@gmail.com>
2022-12-26 08:04:19 +0100

    Initial account data.

---
+++ packages/commons/commons-api/dist/main.f818cd6ab0d03f5fe287.hot-update.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.id = "main";
+exports.ids = null;
+exports.modules = {};
+exports.runtime =
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("c64a826fe37488e40f6f")
+/******/ })();
+/******/ 
+/******/ }
+;
